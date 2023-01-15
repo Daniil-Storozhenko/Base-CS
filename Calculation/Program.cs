cb35901b39fefa83706d9a171752554da8962c57
Андрей Боев <a.boev.it@gmail.com>
2023-01-15 13:47:58 +0300

    Добавлен раздел Calculation

---
+++ Calculation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Корень квадратный
+            Console.WriteLine($"Корень квадратный из 144 = {Math.Sqrt(144)}");
+        }
+    }
+}
